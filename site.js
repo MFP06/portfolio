b7ed74fc8e9ee9150cd090cd3980f6ad3214183d
root <root@DESKTOP-JJH4VS4.localdomain>
2026-03-13 11:20:17 +0100

    update site

---
+++ site.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var currentYear = new Date().getFullYear();
+
+  document.querySelectorAll('[data-current-year]').forEach(function (element) {
+    element.textContent = currentYear;
+  });
+});
